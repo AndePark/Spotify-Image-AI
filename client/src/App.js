6875bb18b39e7bde59ec32fb0ffc6389fe1afd49
Andrew Park <andrewpark2396@gmail.com>
2024-08-22 02:47:15 -0700

    style components added

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,18 @@ import {
   Route, 
   useLocation
 } from 'react-router-dom';
-import './App.css';
+import styled from 'styled-components/macro';
+import { GlobalStyle } from './styles';
+
+
+const StyledLoginButton = styled.a`
+  background-color: var(--green);
+  color: var(--white);
+  padding: 10px 20px;
+  margin: 20px;
+  border-radius: 30px;
+  display: inline-block;
+`;
 
 function ScrollToTop() {
   const {pathname} = useLocation();
@@ -42,11 +53,13 @@ function App() {
 
   return (
     <div className="App">
+      <GlobalStyle />
+
       <header className="App-header">
       {!token ? (
-          <a className="App-link" href="http://localhost:8888/login">
+          <StyledLoginButton href="http://localhost:8888/login">
             Log in to Spotify
-          </a>
+          </StyledLoginButton>
         ) : (
           <Router>
             <ScrollToTop />
